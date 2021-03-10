e1e4e10ad01758ea4e8d37c0dd6ea6a98b604072
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-10 10:37:21 +0800

    Создание табов в проекте

--- js/main_script.js
+++ js/main_script.js
@@ -1,47 +1,40 @@
 "use sctrict";  
 
-const   buttons = document.querySelectorAll('button'),
-        wrapper = document.querySelector('.btn-block');
+window.addEventListener('DOMContentLoaded', () => {
 
-// console.log(buttons[0].classList.length); // Узнать кол-во классов у элемента
-// console.log(buttons[0].classList.item(0)); // Получить наименование класса (по индексу)
-// console.log(buttons[0].classList.add('red')); // Добавить класс у элемента
-// console.log(buttons[0].classList.remove('blue', 'some')); // Удалить класс у элемента
-// console.log(buttons[0].classList.toggle('blue')); // Переключить класс у элемента
+    // Переменные
+    const   tabs = document.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorAll('.tabcontent'),
+            tabsParent = document.querySelector('.tabheader__items');
+    // Функции
+    const   hideTabContent = () => {
+                tabsContent.forEach(item => {
+                    item.classList.add('hide');
+                    item.classList.remove('show', 'fade');
+                });
+                tabs.forEach(item => {
+                    item.classList.remove('tabheader__item_active');
+                });
+            },
 
-// Проверка элемента на наличие класса
-// if (buttons[1].classList.contains('red')) {
-//     console.log('I have class "RED"!');
-// }
+            showTabContent = (i = 0) => {
+                tabsContent[i].classList.add('show', 'fade');
+                tabsContent[i].classList.remove('hide');
+                tabs[i].classList.add('tabheader__item_active');
+            };
+    
+    hideTabContent();
+    showTabContent();
 
-// Добавляем класс через оброботчик событий
+    tabsParent.addEventListener('click', (event) => {
+        if (event.target && event.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (event.target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 
-// Вариант №1
-buttons[1].addEventListener('click', () => {
-    if (!buttons[1].classList.contains('red')) {
-        buttons[1].classList.add('red');
-    } else {
-        buttons[1].classList.remove('red');
-    }
-});
-
-// Вариант №2
-buttons[2].addEventListener('click', () => {
-    buttons[2].classList.toggle('blue');
-});
-
-// -------------- Делегирование событий --------------
-    // добавляется оброботчик событий не на сам элемент, а
-    // на его родителя. Это позволяет динамически добавлять
-    // элементы и буз лишней мароки задавать им ОС*.
-    // *Обработчик событий 
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.classList.contains('blue')) {
-        console.log("Hi, and I`m blue Button.");
-    } else if (event.target && event.target.matches('button.red')) {
-        console.log("А я крассный, и я говорю по русски.");
-    } else if (event.target && event.target.tagName == 'BUTTON') {
-        console.log("Hi, it`s me, Button.");
-    }
 });
