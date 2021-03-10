a2df5b1cb99f6995eff5296a963b21d1f05456f6
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-10 12:53:11 +0800

    Скрипты и время их выполнения. setTimeout и setInterval

--- js/main_script.js
+++ js/main_script.js
@@ -1,40 +1,39 @@
 "use sctrict";  
 
 window.addEventListener('DOMContentLoaded', () => {
-
-    // Переменные
-    const   tabs = document.querySelectorAll('.tabheader__item'),
-            tabsContent = document.querySelectorAll('.tabcontent'),
-            tabsParent = document.querySelector('.tabheader__items');
-    // Функции
-    const   hideTabContent = () => {
-                tabsContent.forEach(item => {
-                    item.classList.add('hide');
-                    item.classList.remove('show', 'fade');
-                });
-                tabs.forEach(item => {
-                    item.classList.remove('tabheader__item_active');
-                });
-            },
-
-            showTabContent = (i = 0) => {
-                tabsContent[i].classList.add('show', 'fade');
-                tabsContent[i].classList.remove('hide');
-                tabs[i].classList.add('tabheader__item_active');
-            };
     
-    hideTabContent();
-    showTabContent();
+    const   btn = document.querySelector('.btn');
+    let     timerID_01,
+            i = 0,
+            id = setTimeout(function log() {
+                console.log('I`m Batman!');
+                id = setTimeout(log, 500);
+            }, 500);
 
-    tabsParent.addEventListener('click', (event) => {
-        if (event.target && event.target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (event.target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
+    btn.addEventListener('click', (event) => {
+        timerID_01 = setInterval(logger02, 2000);
     });
+
+    const   logger01 = () => {
+                console.log('I`m  timerID4.')
+            },
+            logger02 = () => {
+                if (i == 3) clearInterval(timerID_01);
+                console.log('I`m BooBs.');
+                i++;
+            };
+
+    const   timerID1 = setTimeout(function() {
+                console.log('I`m  timerID1');
+            }, 2000),
+            timerID2 = setTimeout(function(text) {
+                console.log(text);
+            }, 4000 ,'I`m  timerID2'),
+            timerID3 = setTimeout(function(text) {
+                console.log(text);
+            }, 5000 ,'I`m  timerID3'),
+            timerID4 = setTimeout(logger01, 6000);
+    
+    clearInterval(timerID3);
 
 });
