9a45ba161b4eed1209b384d88414d665173509e1
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-11 13:24:56 +0800

    функции-конструкторы

--- js/test.js
+++ js/test.js
@@ -1,7 +1,21 @@
 "use sctrict";  
 
 window.addEventListener('DOMContentLoaded', () => {
+    // конструктор
+    function User(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+        this.hello = () => {
+            console.log(`Hello ${this.name}.`)
+        };
+    }
 
-    
+    User.prototype.exit = function() {
+        console.log(`User ${this.name}[${this.id}] has left.`);
+    }
 
+    const Nikita = new User('Nikita', 21);
+    Nikita.hello();
+    Nikita.exit();
 });
