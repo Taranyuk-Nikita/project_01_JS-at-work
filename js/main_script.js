a9fe1cc636d3398c928962ee132e103c5adad6a9
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-12 11:22:25 +0800

    Использование классов в проекте

--- js/main_script.js
+++ js/main_script.js
@@ -114,7 +114,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     clearInterval(modalTimerOpen);
                 }
             },
-            modalTimerOpen = setTimeout(openModal, 60000);   
+            modalTimerOpen = setTimeout(openModal, 120000);   
 
     modalOpen.forEach(btn => {
         btn.addEventListener('click', openModal);
@@ -135,5 +135,65 @@ window.addEventListener('DOMContentLoaded', () => {
            closeModal();
         }
     });
+
+    /* ------------- CARDS ------------*/
+    class MenuCard {
+        constructor(srcImg, alt, title, desrc, price, parentSelector) {
+            this.srcImg = srcImg;
+            this.alt = alt;
+            this.title = title;
+            this.desrc = desrc;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToGrivna();
+        }
+
+        changeToGrivna() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.srcImg} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.desrc}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "/sources/img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+    new MenuCard(
+        "/sources/img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container'
+    ).render();
+    new MenuCard(
+        "/sources/img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container'
+    ).render();
 
 });
--- js/test.js
+++ js/test.js
@@ -1,37 +1,7 @@
 "use strict";  
 
 window.addEventListener('DOMContentLoaded', () => {
-    
-    class Rectangle {
-        constructor(height, width) {
-            this.height = height;
-            this.width = width;           
-        }
 
-        calcArea() {
-            return this.height * this.width;
-        }
-    }
 
-    class ColoredRectangleWithText extends Rectangle {
-        constructor(height, width, text, bgColor) {
-            super(height, width);
-            this.text = text;
-            this.bgColor = bgColor;
-        }
-
-        showMyProps() {
-            console.log(`Текст: ${this.text}, Цвет: ${this.bgColor}`);
-        }
-    }
-
-    const div = new ColoredRectangleWithText(25, 10, "Hi", 'red');
-    div.showMyProps();
-    console.log(div.calcArea());
-
-    const   square = new Rectangle(10, 10);
-    const   square01 = new Rectangle(12, 17);
-    console.log(square.calcArea());
-    console.log(square01.calcArea());
 
 });
