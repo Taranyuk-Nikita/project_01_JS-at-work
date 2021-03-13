1532abdfdd49f42de41f659fcea2915b4338a5da
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-13 14:01:50 +0800

    AJAX и общение с сервером

--- js/test.js
+++ js/test.js
@@ -1,19 +1,39 @@
 "use strict";  
 
-window.addEventListener('DOMContentLoaded', () => {
+const   inputRUB = document.querySelector('#rub'),
+        inputUSD = document.querySelector('#usd'); 
 
-    const persone = {
-        name: 'Nikita',
-        telephone : '+7 999 999 99 99',
-        parants: {
-            mama: 'Lena',
-            dady: 'Igor'
+inputRUB.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, password);
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // request.send(body);
+    request.send();
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+    // request.addEventListener('readystatechange', () => {
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         // console.log(request.response);
+    //         const data = JSON.parse(request.response);
+    //         inputUSD.value = (+inputRUB.value / data.current.usd).toFixed(2);
+    //     } else {
+    //         inputUSD.value = 'Что-то пошло не так...';
+    //     }
+    // });
+
+    request.addEventListener('load', () => {
+        if ( request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUSD.value = (+inputRUB.value / data.current.usd).toFixed(2);
+        } else {
+            inputUSD.value = 'Что-то пошло не так...';
         }
-    };
-    console.log(JSON.parse(JSON.stringify(persone)));
-    const clone = (JSON.parse(JSON.stringify(persone)));
-    clone.parants.mama = "Elena";
-    console.log(clone);
+    });
 
 });
-
