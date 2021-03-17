d53f362e97984f0ec7620907664a4aa969f84040
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-17 23:33:20 +0800

    Получение данных с сервера. Async и await.

--- js/main_script.js
+++ js/main_script.js
@@ -175,32 +175,20 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuCard(
-        "sources/img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
-    ).render();
-    new MenuCard(
-        "sources/img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container',
-        "menu__item", 'big'
-    ).render();
-    new MenuCard(
-        "sources/img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container',
-        "menu__item"
-    ).render();
+    const getResosurce = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) throw new Error(`Could not fetch ${url}, status: ${res.status}.`);
+
+        return await res.json();
+    };
+
+    getResosurce('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
 
     /* ------------ POST FORM ----------*/
     
@@ -209,6 +197,18 @@ window.addEventListener('DOMContentLoaded', () => {
     forms.forEach(item => {
         postForm(item);
     });
+
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
 
     const messages = {
         loading: 'sources/svg/spinner.svg',
@@ -227,19 +227,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksMaodal(messages.success);
