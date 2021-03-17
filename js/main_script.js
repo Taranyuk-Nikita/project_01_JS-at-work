a451f5998d9db3ab64cbe3e21f4fb22ae8ed4272
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-17 22:52:28 +0800

    JSON-server

--- js/main_script.js
+++ js/main_script.js
@@ -280,4 +280,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 5000);
     }
 
-});
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
+}); 
