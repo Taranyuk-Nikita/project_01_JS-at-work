17c589376d801b7c2a2f7d62a14ad1f873e98229
Taranyuk Nikita <geargamer3112@gmail.com>
2021-03-19 08:50:15 +0800

    Слайдер. Вариант 1.

--- js/main_script.js
+++ js/main_script.js
@@ -274,4 +274,46 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(data => data.json())
         .then(res => console.log(res));
 
+    /* ------------ SLIDER-01 ----------*/
+
+    const   slides = document.querySelectorAll('.offer__slide'),
+            prev = document.querySelector('.offer__slider-prev'),
+            next = document.querySelector('.offer__slider-next'),
+            total = document.querySelector('#total'),
+            current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    const showSlides = (n) => {
+        if (n > slides.length) slideIndex = 1;
+        if (n < 1) slideIndex = slides.length;
+
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        };
+    };
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    };
+
+    const nextSlide = (n) => {
+        showSlides(slideIndex += n);
+    }; 
+
+    prev.addEventListener('click', () => {
+        nextSlide(-1);
+    });
+    next.addEventListener('click', () => {
+        nextSlide(1);
+    });
+        
 }); 
